b5886932ef839b7909429ec758d160c6f0cbed4c
TsukasaDEKA <deka1066@gmail.com>
2019-12-25 10:21:25 +1100

    Change using db to MySQL.

--- dominion_randomizer/dominion_randomizer/settings.py
+++ dominion_randomizer/dominion_randomizer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dominion_randomizer'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,13 @@ WSGI_APPLICATION = 'dominion_randomizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dominion_randomizer_w_django',
+        'USER': 'dominion_user',
+        'PASSWORD': 'dominion_randomizer_w_django',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- dominion_randomizer/manage.py
+++ dominion_randomizer/manage.py
@@ -13,3 +13,5 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    import pymysql
+    pymysql.install_as_MySQLdb()
