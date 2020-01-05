28856be50b6b32b8cd6922e9ef27eb1b82cab2c4
TsukasaDEKA <deka1066@gmail.com>
2020-01-05 23:56:41 +1100

    Add expantion data

--- dominion_randomizer/dominion_randomizer/models/expansion.py
+++ dominion_randomizer/dominion_randomizer/models/expansion.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Expantion(models.Model):
+class Expansion(models.Model):
     name_jp_kanji = models.CharField(max_length=30)
     name_jp_pronunciations = models.CharField(max_length=30)
     name_en = models.CharField(max_length=60)
 
     class Meta:
-        db_table = 'expantions'
+        db_table = 'expansions'
